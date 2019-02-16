caa011d45baea8a9ab06036de32b296ab02cbaa3
wolfph <inf2323@hs-worms>
2019-02-16 19:57:46 +0100

    added math operations

--- giraffe.rb
+++ giraffe.rb
@@ -1,3 +1,11 @@
 phrase = "Hello World"
-puts phrase[4, 7]
-puts phrase.index("l")
+#puts phrase[4, 7]
+#puts phrase.index("l")
+
+num = 20.687
+puts ("my fav number is " + num.to_s)
+puts num.round()
+puts num.ceil() #the next higher number
+puts num.floor() #cut of after the dot
+puts Math.sqrt(36)
+puts Math.log(5) #logar
