6826dc7843a8ab524b956eb290be16e0c422e151
root <root@localhost.localdomain>
2019-02-18 19:21:35 +0100

    fixed the divid bug

--- calculator.rb
+++ calculator.rb
@@ -10,7 +10,7 @@ def calculator(num1, num2)
   exp = num1 ** num2
   puts ("The Sum is: " + sum.to_s)
   puts ("The Product is: " + prod.to_s)
-  puts ("The divided result is: " + qut.to_s)
+  puts ("The divided result is: " + div.to_s)
   puts ("The Power of is: " + exp.to_s)
 end
 
